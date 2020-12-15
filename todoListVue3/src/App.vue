71d4a1952a04d287075617ec1fc77cf6df72456c
Kyle-Alpha <1270112884@qq.com>
2020-12-15 13:44:35 +0800

    todo

--- todoListVue3/src/App.vue
+++ todoListVue3/src/App.vue
@@ -1,55 +1,85 @@
 <template>
-  <el-row :gutter="20" >
+  <el-row>
     <el-col :span="12" :offset="6" class="flex">
-      <el-input v-model="input" placeholder="请输入内容"/>  
-       <el-button type="success">确认</el-button>
-      </el-col>
-    <!-- <el-col :span="4" >
-      <el-button type="success">确认</el-button>
-      </el-col> -->
+      <el-input v-model="wantTodo" placeholder="请输入内容" />
+      <el-button type="primary" plain @click="handleAddToDo">添加</el-button>
+    </el-col>
   </el-row>
- 
+  <el-row>
+    <el-col :span="12" :offset="6">
+      <to-do :todoit="todoList" @del-todo="delTodo"></to-do>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import {onMounted,watch,computed,reactive,ref,toRefs,nextTick} from 'vue'
+import ToDo from './components/toDoIt.vue'
+import {
+  onMounted,
+  watch,
+  computed,
+  reactive,
+  ref,
+  toRefs,
+  nextTick
+} from 'vue'
 import {
   ElInput,
   ElButton,
   ElRow,
-  ElCol
+  ElCol,
+  ElCard,
+  Message
   // ...
 } from 'element3'
+import ToDoIt from './components/toDoIt.vue'
+const AddtoDo = () => {
+  const states = reactive({
+    wantTodo: '',
+    todoList: []
+  })
+  const handleAddToDo = () => {
+    let { wantTodo, todoList } = states
+    if (!wantTodo) {
+      return Message.info('请填写后在添加')
+    }
+    if (todoList.includes(wantTodo)) return Message.info('请勿添加重复事宜')
+    const id = todoList.length
+    const isChecked = false
+    todoList.unshift({
+      id,
+      isChecked,
+      wantTodo
+    })
+    states.wantTodo = ''
+  }
+  return {
+    handleAddToDo,
+    states
+  }
+}
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo
   },
-  setup(prop,ctx){
-    const data = reactive({
-      name:'kyle',
-      age:24,
-      job:'IT'
-    })
-    const msg = ref('')
-    const input = ref(null)
-    const str = computed(()=>msg.value.split('').reverse().join(''))
-    onMounted(()=>{
-     
-    })
+  setup() {
+    const { handleAddToDo, states } = AddtoDo()
+    const delTodo = (val) => {
+      states.todoList.splice(val, 1)
+    }
     return {
-      msg,
-      data,
-      input,
-      str
+      handleAddToDo,
+      ...toRefs(states),
+      delTodo
     }
   }
 }
 </script>
 <style lang="">
-  .flex{
-    display: flex;
-    justify-content: space-around;
-  }
+.flex {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
---
+++ todoListVue3/src/components/toDoIt.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-card class="box-card">
+    <div>
+      <div v-for="(item, index) in todoit" :key="item.id" class="item">
+        <el-checkbox
+          v-model="item.isChecked"
+          :class="{ 'had-done': item.isChecked }"
+          >{{ item.wantTodo }}</el-checkbox
+        >
+        <i class="el-icon-error" @click="delTodo(index)"></i>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import { ref, watch, onUpdated } from 'vue'
+export default {
+  props: {
+    todoit: Array
+  },
+  emits: ['del-todo'],
+  setup(props, ctx) {
+    const delTodo = (i) => {
+      ctx.emit('del-todo', i)
+    }
+    return {
+      delTodo
+    }
+  }
+}
+</script>
+
+<style>
+.box-card {
+  width: 100%;
+  height: 300px;
+}
+.item {
+  display: flex;
+  background-color: #eff;
+  border-bottom: 1px solid #999;
+  padding: 6px 15px;
+  border-radius: 4px;
+  box-shadow: 1px 10px 10px #ccc;
+  margin-bottom: 4px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #666;
+  position: relative;
+  overflow: hidden;
+}
+.item:hover i {
+  opacity: 1;
+  transition: all 1s ease;
+}
+.item > i {
+  color: #f00;
+  opacity: 0;
+}
+.item::before {
+  position: absolute;
+  content: '';
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 3px;
+  height: 100%;
+  background-color: #f00;
+}
+
+.had-done > .el-checkbox__label {
+  color: #ccc !important;
+  text-decoration: line-through;
+}
+</style>
